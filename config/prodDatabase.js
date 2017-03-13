1d0e28e21f257a8db57ea10efdc6eff31381af38
Talysson <talyssonoc@gmail.com>
2017-03-13 18:52:45 -0300

    Change env var passing

--- config/prodDatabase.js
+++ config/prodDatabase.js
@@ -1,8 +1,3 @@
-console.log(process.env.DATABASE_URL);
-
 module.exports = {
-  production: {
-    dialect: 'postgres',
-    url: process.env.DATABASE_URL
-  }
+  production: process.env.DATABASE_URL
 };
