90c68543a1f6c9eeaace74a035da1ca287918200
Talysson <talyssonoc@gmail.com>
2017-03-13 19:05:51 -0300

    Put all db variables on ENV

--- config/prodDatabase.js
+++ config/prodDatabase.js
@@ -1,3 +1,14 @@
+const {
+  DB_USER: username,
+  DB_PWD: password,
+  DB_NAME: database,
+  DB_HOST: host
+} = process.env;
+
 module.exports = {
-  production: process.env.DATABASE_URL
+  production: {
+    username, password,
+    database, host
+    dialect: 'postgres'
+  }
 };
