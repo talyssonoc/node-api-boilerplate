7d21ebe63733310fe2178d6fb23eb4742cf1c1f3
Talysson <talyssonoc@gmail.com>
2017-03-15 14:43:40 -0300

    Improve database config loading

--- config/index.js
+++ config/index.js
@@ -4,11 +4,12 @@ const path = require('path');
 const ENV = process.env.NODE_ENV || 'development';
 
 const envConfig = require(path.join(__dirname, 'environments', ENV));
+const dbConfig = process.env.DATABASE_URL || require('./database')[ENV];
 
 const config = Object.assign({
   [ENV]: true,
   env: ENV,
-  db: require(ENV === 'production' ? './prodDatabase' : './database')[ENV]
+  db: dbConfig
 }, envConfig);
 
 module.exports = config;
