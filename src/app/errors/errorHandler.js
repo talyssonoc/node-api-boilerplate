3fd273b947907017b47960c95432eda5d90b5dc7
Talysson <talyssonoc@gmail.com>
2017-03-16 08:32:56 -0300

    Add error logging on error handlers

--- src/app/errors/errorHandler.js
+++ src/app/errors/errorHandler.js
@@ -2,6 +2,10 @@ const Status = require('http-status');
 
 /* istanbul ignore next */
 module.exports = (err, req, res, next) => {
+  const { logger } = req.container.cradle;
+
+  logger.error(err);
+
   res.status(Status.INTERNAL_SERVER_ERROR).json({
     type: 'InternalServerError',
     message: 'The server failed to handle this request'
