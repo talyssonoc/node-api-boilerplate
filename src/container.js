2290aa9b1b97cd690389e921d19a9fba0df685b2
Talysson <talyssonoc@gmail.com>
2017-03-21 22:51:04 -0300

    Move the console to an interface

--- src/container.js
+++ src/container.js
@@ -3,13 +3,16 @@ const { scopePerRequest } = require('awilix-express');
 
 const config = require('../config');
 const Application = require('./app/Application');
-const Server = require('./app/Server');
 const logger = require('./app/logging/logger');
+
+const Server = require('./interfaces/http/Server');
 const router = require('./interfaces/http/router');
 const loggerMiddleware = require('./interfaces/http/logging/loggerMiddleware');
 const errorHandler = require('./interfaces/http/errors/errorHandler');
 const devErrorHandler = require('./interfaces/http/errors/devErrorHandler');
+
 const { CreateUser, GetAllUsers } = require('./domain/user/operations');
+
 const SequelizeUsersRepository = require('./infra/user/SequelizeUsersRepository');
 const { database, User: UserModel } = require('./infra/database/models');
 
--- src/interfaces/console/index.js
+++ src/interfaces/console/index.js
@@ -1,6 +1,6 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const Console = require('src/app/Console');
+const Console = require('./Console');
 const container = require('src/container');
 
 Console.start({
