08a388472705bc3f7738037bbd73747b853b0f4c
Bruno Henrique de Castro <brunohcastro@hotmail.com>
2021-08-22 20:44:24 -0300

    Add logging messages to message bundle

--- src/messages.ts
+++ src/messages.ts
@@ -7,5 +7,7 @@ const messageBundle = makeMessegeBundle<Messages>();
 
 const { getMessage, updateBundle, useBundle } = messageBundle;
 
+type MessageBundle = typeof messageBundle;
+
 export { messageBundle, getMessage, updateBundle, useBundle };
-export type { Messages };
+export type { Messages, MessageBundle };
