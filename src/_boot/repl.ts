fbd6d86ec97a648d94f606c33cadb279c597a773
Jeferson S. Brito <jefersonsilva4k@hotmail.com>
2021-10-10 00:19:15 -0300

    Extract REPL impl. to lib

--- src/_boot/repl.ts
+++ src/_boot/repl.ts
@@ -1,7 +1,6 @@
-import REPL, { REPLEval, ReplOptions, REPLServer } from 'repl';
-import vm from 'vm';
-import { createServer, Server } from 'net';
 import { makeModule } from '@/context';
+import { EnvironmentConfig } from '@/_lib/Environment';
+import { makeNodeREPL } from '@/_lib/repl';
 
 type REPLConfig = {
   appName: string;
@@ -9,90 +8,20 @@ type REPLConfig = {
   repl: {
     port: number;
   };
+  environment: EnvironmentConfig['environment'];
 };
 
-const repl = makeModule(
-  'repl',
-  async ({
-    app: { onReady, terminate },
-    container,
-    config: {
-      appName,
-      cli,
-      environment,
-      repl: { port },
-    },
-    logger,
-  }) => {
-    const promisableEval: REPLEval = (cmd, context, filename, callback) => {
-      const result = vm.runInContext(cmd, context);
+const repl = makeModule('repl', async ({ app: { onReady, terminate }, container, config, logger }) => {
+  const repl = makeNodeREPL({ container, config, logger });
 
-      if (isPromise(result)) {
-        return result.then((v) => callback(null, v)).catch((e) => callback(e, null));
-      }
+  onReady(async () => {
+    await repl.start({ terminate });
+  });
 
-      return callback(null, result);
-    };
-
-    const isPromise = (value) => value && typeof value.then === 'function' && typeof value.catch === 'function';
-
-    const createREPL = (
-      config: Partial<ReplOptions> = { input: process.stdin, output: process.stdout }
-    ): REPLServer => {
-      const repl = REPL.start({
-        eval: promisableEval,
-        prompt: `${appName}$ `,
-        ignoreUndefined: true,
-        ...config,
-      });
-
-      Object.assign(repl.context, { registry: container.cradle, container });
-
-      return repl;
-    };
-
-    let server: Server;
-
-    const startREPL = async () => {
-      if (cli) {
-        const repl = createREPL();
-
-        repl.on('close', terminate);
-      } else if (!['production', 'test'].includes(environment)) {
-        server = createServer((socket) => {
-          const repl = createREPL({
-            input: socket,
-            output: socket,
-            terminal: true,
-          });
-
-          repl.on('close', () => {
-            socket.end();
-          });
-
-          socket.on('error', (err) => {
-            logger.error('[REPL] Connection error');
-            logger.error(err);
-            socket.end();
-          });
-        }).listen(port);
-      }
-    };
-
-    onReady(startREPL);
-
-    return async () => {
-      if (server && server.listening) {
-        await new Promise<void>((resolve, reject) =>
-          server.close((err) => {
-            if (err) return reject(err);
-            resolve();
-          })
-        );
-      }
-    };
-  }
-);
+  return async () => {
+    await repl.close();
+  };
+});
 
 export { repl };
 export type { REPLConfig };
