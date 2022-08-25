13080f5839aa4a100ff0c270afa88cf17da5bc66
Bruno Castro <henrique.castro@codeminer42.com>
2022-08-25 11:46:06 -0300

    Fix request token typing declaration

--- typings.d.ts
+++ typings.d.ts
@@ -2,5 +2,6 @@ declare module 'http' {
   export interface IncomingMessage {
     id?: string;
     container: import('@/container').Container;
+    token?: { credentials: import('@/_lib/security/Credentials').Credentials.Type };
   }
 }
